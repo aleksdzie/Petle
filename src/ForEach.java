a296111b356a7febbaeb8075814042017ee375ce
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-11-23 19:24:00 +0100

    Creating 'For each' loop

---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args){
+        String[] Cats=new String[]{"Burek", "Mruczek", "Bonifacy", "Klakier"};
+        for (String Cat:Cats) {
+            System.out.println(Cat);
+        }
+    }
+}
